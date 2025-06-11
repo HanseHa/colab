d27dfaf8fb844373c7307ebee1fce5b3efb87350
HanseHa <30470608+HanseHa@users.noreply.github.com>
2025-06-11 08:15:57 +0000

    format fix

--- wikibot.py
+++ wikibot.py
@@ -1,15 +1,14 @@
 import click
 from mylib.bot import scrape
+
 # This script uses the Wikipedia API to scrape summaries of given topics.
 # It uses the Click library to create a command-line interface for the script.
 # The script defines a command `main` that takes two arguments: `name` and `length`.
 
-@click.command()
-@click.option("--name", 
-              help='The topic to search for on Wikipedia.')
-@click.option("--length", 
-              help='The length of the wikipedia output.')
 
+@click.command()
+@click.option("--name", help="The topic to search for on Wikipedia.")
+@click.option("--length", help="The length of the wikipedia output.")
 def cli(name, length):
     """
     Scrape a summary from Wikipedia for the given topic.
